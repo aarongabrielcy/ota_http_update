64e6f0e5b73aa25fe111d8bfa327ca9369418ec5
dexse0t1QZF70xwy* <becario@3code.us>
2025-11-12 17:23:26 -0600

    ota update

--- components/uart/include/uartProcess.h
+++ components/uart/include/uartProcess.h
@@ -2,9 +2,17 @@
 /*#include "esp_err.h"
 #include "esp_ota_ops.h"*/
 
+typedef enum{
+    UART_STATE_IDLE,
+    UART_STATE_OTA
+    //UART_PREPARE_OTA
+} uart_state_t;
+
 void uartSim_task(void *arg);
 void serialConsole_task(void *arg);
 
 int  uartManager_readBinary(uint8_t *buffer, int max_length, int timeout_ms);
 int  uartManager_readEvent(   char *buffer, int max_length, int timeout_ms);
 void uartProcess_sendCommand(const char *command);
+
+void setUart_state(uart_state_t state);
